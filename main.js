41a8a376e3ebbccf404cd45a6fa09a14dce6382a
viz-nu/ <vishnu.teja101.vt@gmail.com>
2023-04-06 22:58:14 +0530

    road unfinished

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const canvas = document.getElementById("myCanvas");
 
-canvas.width = 800;
+canvas.width = 500;
 
 const ctx = canvas.getContext("2d");
-const car = new Car(100,100,30,50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9)
+const car = new Car(road.getLaneCenter(0), 100, 30, 50);
 
 Animate();
 function Animate() {
     car.update()
     canvas.height = window.innerHeight;
+    road.draw(ctx)
     car.draw(ctx)
     requestAnimationFrame(Animate);
 }
